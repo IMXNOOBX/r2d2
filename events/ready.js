9d0501d12b9aec380c2ec9eec825d0ff81ca2dc8
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-02-09 13:19:56 +0100

    Refactor code for improved performance

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+module.exports.run = async (client) => {
+  client.log.console(
+    `[BOT] | Username: ${client.user.tag} | Guilds: ${client.guilds.cache.size} servers | Users: ${client.users.cache.size} total users`
+  );
+  client.user.setStatus("dnd"); // online, idle, dnd, invisible
+
+  const set_activity = async() => {
+    const statuses = [
+      `${client.guilds.cache.size} Guilds`,
+      `${client.users.cache.size} Users`,
+      `${client.emojis.cache.size} Emojis`,
+      `github.com/IMXNOOBX`,
+    ];
+
+    const status = statuses[Math.floor(Math.random() * statuses.length)]; //Easy way to make random dynamic statuses
+    client.user.setActivity(status, { type: client.discord.ActivityType.Watching }); //LISTENING, WATCHING, PLAYING
+  }
+
+  setInterval(set_activity, 60000); set_activity(); 
+};
